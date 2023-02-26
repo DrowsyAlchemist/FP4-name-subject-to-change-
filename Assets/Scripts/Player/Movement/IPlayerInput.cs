966dc1d5ba65bf970c5d1c660f75a22133b89deb
Neto test <michaelGulidow@yandex.ru>
2023-02-26 21:30:11 +0300

    Input from touch added

--- Assets/Scripts/Player/Movement/IPlayerInput.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public interface IPlayerInput
-{
-    public Vector3 GetDirection();
-}
--- Assets/Scripts/Player/Movement/PlayerInput.cs
+++ Assets/Scripts/Player/Movement/PlayerInput.cs
@@ -1,6 +1,70 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public abstract class PlayerInput : MonoBehaviour, IPlayerInput
+public class PlayerInput : MonoBehaviour, IPointerDownHandler, IPointerExitHandler, IPointerEnterHandler
 {
-    public abstract Vector3 GetDirection();
+    [SerializeField] private Transform _player;
+    [SerializeField] private Camera _camera;
+
+    private const float Epsilon = 0.2f;
+
+    private bool _isTouchInput;
+    private RaycastHit _hit;
+    private LayerMask _layerMask;
+    private Ray _ray;
+
+    private void Awake()
+    {
+        _layerMask = 1 << gameObject.layer;
+    }
+
+    public Vector3 GetDirection()
+    {
+        if (_isTouchInput && (Input.touchCount > 0 || Input.GetMouseButton(0)))
+            return GetDirectionFromPointer();
+        else
+            return GetDirectionFromKeyboard();
+    }
+
+    private Vector3 GetDirectionFromKeyboard()
+    {
+        if (Input.GetKey(KeyCode.D))
+            return Vector3.right;
+
+        if (Input.GetKey(KeyCode.A))
+            return Vector3.left;
+
+        return Vector3.zero;
+    }
+
+    private Vector3 GetDirectionFromPointer()
+    {
+        _ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(_ray, out _hit, Mathf.Infinity, _layerMask))
+        {
+            Vector3 targetPosition = _hit.point;
+            float delta = targetPosition.x - _player.position.x;
+
+            if (Mathf.Abs(delta) > Epsilon)
+                return delta * Vector3.right;
+        }
+        return Vector3.zero;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _isTouchInput = true;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (Input.GetMouseButton(0))
+            _isTouchInput = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _isTouchInput = false;
+    }
 }
